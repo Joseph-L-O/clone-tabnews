5a956a0299c31f0d94dcef2291cf885389ac2147
Joseph Leonardo de olveira <josephleonardodeoliveira@gmail.com>
2025-11-07 12:11:51 +0000

    adicionando o arquivo  e removendo arquivos  atuais do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,5 @@
-// pages/index.js
-
-import React from 'react';
-import ImageGlitchWithQuote from './ImageGlitchWithQuote';
-
 const HomePage = () => {
-    return (
-        <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <ImageGlitchWithQuote
-                src="/elevate.jpeg"
-                idleSrc="/elevate.jpeg"  
-                alt="Imagem com efeito glitch"
-                glitchFrames={[
-                    "/elevate2.png",
-                    "/elevate3.png",
-                    "/elevate4.png",
-                    "/elevate5.png"
-                ]}
-            />
-
-
-        </main>
-    );
+    return <></>;
 };
 
 export default HomePage;
